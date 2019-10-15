daadc28604a0e1e28149f4d49df831c0c1869661
gaganVirk <deep.virk@outlook.co.nz>
2019-10-15 16:04:29 +1300

    checks weighted list with list

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace WeightAdjacencyList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TextReader reader = Console.In;
+            //Console.SetIn(new StreamReader("weightAdjList.txt"));
+
+            List<Vertex> vertices = new List<Vertex>();
+            Vertex v1 = new Vertex(0);
+
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0; i < size; i++)
+            {
+                vertices.Add(new Vertex(i));
+            }
+
+            string line = Console.ReadLine();
+            while(line != "#")
+            {
+                string[] array = line.Split(' ');
+                int token1 = int.Parse(array[0]);
+                int token2 = int.Parse(array[1]);
+                int cost = int.Parse(array[2]);
+
+                vertices[token1].Adj.Add(vertices[token2]);
+                vertices[token1].Weights.Add(vertices[token2], cost);
+
+                line = Console.ReadLine();
+            }
+
+            foreach (var e in vertices)
+            {
+                   string s = e.Id + ": ";
+
+                foreach (var d in e.Adj)
+                {
+                    s += d.Id + "(" + e.Weights[d] + ") ";
+
+                }
+                Console.WriteLine(s);
+            }
+
+
+          // Console.SetIn(reader);
+           // Console.ReadLine();
+        }
+    }
+}
---
+++ Vertex.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeightAdjacencyList
+{
+    class Vertex
+    {
+        public int Id { get; set; }
+
+        public Dictionary<Vertex, int> Weights = new Dictionary<Vertex, int>();
+
+        public List<Vertex> Adj = new List<Vertex>();
+
+        public Vertex(int id)
+        {
+            Id = id;
+        }
+
+        
+
+    }
+}
